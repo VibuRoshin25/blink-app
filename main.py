df579a82e72e7c7f3896ba50cc5cefe09f854515
Vibu Roshin E V <87224190+VibuRoshin25@users.noreply.github.com>
2023-10-10 15:48:59 +0530

    Update main.py
    UI added

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import cv2
 import dlib
 import math
 from datetime import datetime
+import tkinter as tk
+from tkinter import messagebox, font
+from PIL import Image, ImageTk
 
-# Constants
 BLINK_RATIO_THRESHOLD = 4
 BLINK_DURATION = 60  # Duration in seconds
-COOLDOWN_TIME = 0.2  # Cooldown time in seconds
-
+COOLDOWN_TIME = 1.5  # Cooldown time in seconds
+MIN_BLINK_COUNT = 15
 
 def midpoint(point1, point2):
     return (point1.x + point2.x) / 2, (point1.y + point2.y) / 2
 
-
 def euclidean_distance(point1, point2):
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
-
 
 def get_blink_ratio(eye_points, facial_landmarks):
     corner_left = (
@@ -41,10 +41,8 @@ def get_blink_ratio(eye_points, facial_landmarks):
 
     return ratio
 
-
 # OpenCV setup
 cap = cv2.VideoCapture(0)
-cv2.namedWindow("BlinkDetector")
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
@@ -55,62 +53,118 @@ blink_counter = 0
 start_time = datetime.now()
 last_blink_time = start_time
 
-while True:
-    retval, frame = cap.read()
+def show_popup_message(message):
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    messagebox.showinfo("Blink Alert", message)
+    root.destroy()
 
-    if not retval:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+def update_frame():
+    global last_blink_time, blink_counter, start_time  # Declare last_blink_time and blink_counter as global variables
+    current_time = datetime.now()  # Initialize current_time here
+    ret, frame = cap.read()
+    if ret:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        frame = cv2.GaussianBlur(frame, (5, 5), 0)
+        
+        photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+        label.config(image=photo)
+        label.image = photo
 
-    # Flip the frame horizontally
-    frame = cv2.flip(frame, 1)
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        faces, _, _ = detector.run(image=frame_gray, upsample_num_times=0, adjust_threshold=0.0)
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces, _, _ = detector.run(image=frame, upsample_num_times=0, adjust_threshold=0.0)
+        for face in faces:
+            landmarks = predictor(frame_gray, face)
 
-    for face in faces:
-        landmarks = predictor(frame, face)
+            left_eye_ratio = get_blink_ratio(left_eye_landmarks, landmarks)
+            right_eye_ratio = get_blink_ratio(right_eye_landmarks, landmarks)
+            blink_ratio = (left_eye_ratio + right_eye_ratio) / 2
 
-        left_eye_ratio = get_blink_ratio(left_eye_landmarks, landmarks)
-        right_eye_ratio = get_blink_ratio(right_eye_landmarks, landmarks)
-        blink_ratio = (left_eye_ratio + right_eye_ratio) / 2
+            current_time = datetime.now()
 
-        current_time = datetime.now()
+            elapsed_time = (current_time - start_time).total_seconds()
 
-        if (
-            blink_ratio > BLINK_RATIO_THRESHOLD
-            and (current_time - last_blink_time).total_seconds() >= COOLDOWN_TIME
-        ):
-            blink_counter += 1
-            last_blink_time = current_time
+            if elapsed_time >= BLINK_DURATION:
+                # Calculate and print the average blink count
+                average_blinks = blink_counter / BLINK_DURATION
+                print(f"Average Blinks in {BLINK_DURATION} seconds: {average_blinks:.2f}")
 
-        # Display blink count and elapsed time
-        blink_text = f"Blink Count: {blink_counter}"
-        elapsed_time = (current_time - start_time).total_seconds()
-        elapsed_time_text = f"Elapsed Time: {elapsed_time:.2f}s"
-        cv2.putText(
-            frame,
-            blink_text,
-            (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.7,
-            (255, 255, 255),
-            2,
-        )
-        cv2.putText(
-            frame,
-            elapsed_time_text,
-            (10, 60),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.7,
-            (255, 255, 255),
-            2,
-        )
+                # Reset elapsed time and blink count
+                start_time = current_time
 
-    cv2.imshow("BlinkDetector", frame)
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+                if blink_counter < MIN_BLINK_COUNT:
+                    message = f"Low Blink Count ({average_blinks:.2f}) after {BLINK_DURATION} seconds!"
+                    show_popup_message(message)
+                blink_counter = 0
+
+            if (
+                blink_ratio > BLINK_RATIO_THRESHOLD
+                and (current_time - last_blink_time).total_seconds() >= COOLDOWN_TIME
+            ):
+                blink_counter += 1
+                last_blink_time = current_time
+    # Display blink count and elapsed time in Tkinter labels
+    blink_text = f"Blink Count: {blink_counter}"
+    elapsed_time = (current_time - start_time).total_seconds()
+    elapsed_time_text = f"Elapsed Time: {elapsed_time:.2f}s"
+    blink_label.config(text=blink_text)
+    elapsed_time_label.config(text=elapsed_time_text)
+
+    if not paused:
+        label.after(10, update_frame)  # Refresh every 10 milliseconds
+
+def start_capture():
+    global cap, paused, blink_counter, start_time, last_blink_time
+    if not cap.isOpened():
+        cap = cv2.VideoCapture(0)
+    paused = False
+    blink_counter = 0
+    start_time = datetime.now()
+    last_blink_time = start_time
+    update_frame()
+
+def stop_capture():
+    global cap, paused
+    cap.release()
+    paused = True
+    label.config(image=None)
+
+root = tk.Tk()
+root.title("Blink Detector")
+
+window_width = 640
+window_height = 480
+root.geometry(f"{window_width}x{window_height}")
+
+label_width = 520
+label_height = 400
+label = tk.Label(root, width=label_width, height=label_height)
+label.pack()
+
+blink_label = tk.Label(root, text="Blink Count: 0")
+blink_label.pack()
+
+elapsed_time_label = tk.Label(root, text="Elapsed Time: 0.00s")
+elapsed_time_label.pack()
+
+button_width = 20
+button_height = 4
+
+button_font_style = font.Font(size=14)
+
+start_button = tk.Button(root, text="Start", command=start_capture, width=button_width, height=button_height, font=button_font_style, fg="green")
+stop_button = tk.Button(root, text="Stop", command=stop_capture, width=button_width, height=button_height, font=button_font_style, fg="red")
+
+start_button.pack(side=tk.LEFT)
+stop_button.pack(side=tk.RIGHT)
+
+paused = False
+
+update_frame()
+
+root.mainloop()
 
 cap.release()
 cv2.destroyAllWindows()
